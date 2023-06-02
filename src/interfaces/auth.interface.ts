3bec1bea9a17686e78b390a8ab6561698baf569e
tranvuongduy2003 <tranvuongduy2003@gmail.com>
2023-06-02 15:59:32 +0700

    refersh token

--- src/interfaces/auth.interface.ts
+++ src/interfaces/auth.interface.ts
@@ -5,14 +5,24 @@ export enum Role {
   ADMIN = 'admin',
   USER = 'user',
 }
+export enum TokenType {
+  REFRESH = 'refresh',
+  ACCESS = 'access',
+}
 export interface DataStoredInToken {
   id: number;
   role: Role;
+  type: TokenType;
 }
 
 export interface TokenData {
   token: string;
   expiresIn: number;
+}
+
+export interface TokenPayload {
+  accessToken: string;
+  refreshToken: string;
 }
 
 export interface RequestWithUser extends Request {

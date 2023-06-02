b40d15bc0cd8bb573894cd073b570f684e8eea1c
Ngo Hiep <hoanghiephai@gmail.com>
2023-06-02 09:07:44 +0700

    update route

--- src/routes/products.route.ts
+++ src/routes/products.route.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import {  ProductController } from '@/controllers/products.controller';
+import { ProductController } from '@/controllers/products.controller';
 import { CreateProductDto } from '@/dtos/products.dto';
 import { Routes } from '@interfaces/routes.interface';
 import { ValidationMiddleware } from '@middlewares/validation.middleware';
@@ -7,17 +7,17 @@ import { ValidationMiddleware } from '@middlewares/validation.middleware';
 export class ProductRoute implements Routes {
   public path = '/products';
   public router = Router();
-  public user = new ProductController();
+  public product = new ProductController();
 
   constructor() {
     this.initializeRoutes();
   }
 
   private initializeRoutes() {
-    this.router.get(`${this.path}`, this.user.getProducts);
-    this.router.get(`${this.path}/:id(\\d+)`, this.user.getProductById);
-    this.router.post(`${this.path}`, ValidationMiddleware(CreateProductDto), this.user.createProduct);
-    this.router.put(`${this.path}/:id(\\d+)`, ValidationMiddleware(CreateProductDto, true), this.user.updateProduct);
-    this.router.delete(`${this.path}/:id(\\d+)`, this.user.deleteProduct);
+    this.router.get(`${this.path}`, this.product.getProducts);
+    this.router.get(`${this.path}/:id(\\d+)`, this.product.getProductById);
+    this.router.post(`${this.path}`, ValidationMiddleware(CreateProductDto), this.product.createProduct);
+    this.router.put(`${this.path}/:id(\\d+)`, ValidationMiddleware(CreateProductDto, true), this.product.updateProduct);
+    this.router.delete(`${this.path}/:id(\\d+)`, this.product.deleteProduct);
   }
 }
